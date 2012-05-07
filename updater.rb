5f7773ce24a8d8601343895a4a0ec1e91e1c5c5e
Fabian Schlenz <mail@fabianonline.de>
2012-05-07 10:41:08 +0200

    Updater-Code massively consolidated.

--- updater.rb
+++ updater.rb
@@ -1,6 +1,36 @@
 require 'twitter_oauth'
 $stdout.sync = true
 
+def get_tweets(client, settings={})
+	raise "Missing parameters!" unless [:start, :description, :twitter_method, :tweet_add_method].all?{|e| settings.has_key?(e)}
+
+	print "#{settings[:description].ljust(25)}: "
+	parameters = {"count"=>"200", "since_id"=>settings[:start]}
+	min_id = nil
+	counter = 0
+	objects = []
+    begin
+        temp = client.send(settings[:twitter_method], parameters)
+		print '#'
+		if temp.kind_of?(Array)
+			temp.each do |t|
+				tweet = Tweet.send(settings[:tweet_add_method], t)
+				print "."
+				counter+=1
+				if min_id==nil || min_id>tweet.tweet_id
+					min_id = tweet.tweet_id
+				end
+				objects << tweet
+			end
+		else
+			raise "Got error. Aborting."
+		end
+		parameters["max_id"] = min_id-1 rescue nil
+    end while temp.kind_of?(Array) && temp.length > 0 && min_id!=nil
+	puts " (#{counter})"
+	return objects
+end
+
 def do_update(config)
     client = TwitterOAuth::Client.new(
         :consumer_key =>    config['twitter']['consumer']['token'],
@@ -13,140 +43,23 @@ def do_update(config)
         Process.exit
     end
 
-	objects = []
-
+	all_objects = []
 
     start = Tweet.maximum('tweet_id', :conditions=>'dm = 0') || 1
 
-    print "Timeline:             "
-	parameters = {"count"=>"200", "since_id"=>start}
-	min_id = nil
-	counter = 0
-    begin
-        temp = client.home_timeline(parameters)
-		print '#'
-		if temp.kind_of?(Array)
-			temp.each do |t|
-				tweet = Tweet.add(t)
-				print "."
-				counter+=1
-				if min_id==nil || min_id>tweet.tweet_id
-					min_id = tweet.tweet_id
-				end
-				objects << tweet
-			end
-		else
-			raise "Got error. Aborting."
-		end
-		parameters["max_id"] = min_id-1 rescue nil
-    end while temp.kind_of?(Array) && temp.length > 0 && min_id!=nil
-	puts " (#{counter})"
-
-
-    print "Mentions:             "
-	parameters = {"count"=>"200", "since_id"=>start}
-	min_id = nil
-	counter = 0
-    begin
-        temp = client.mentions(parameters)
-		print '#'
-		if temp.kind_of?(Array)
-			temp.each do |t|
-				tweet = Tweet.add(t)
-				print "."
-				counter+=1
-				if min_id==nil || min_id>tweet.tweet_id
-					min_id = tweet.tweet_id
-				end
-				objects << tweet
-			end
-		else
-			raise "Got error. Aborting."
-		end
-		parameters["max_id"] = min_id-1 rescue nil
-    end while temp.kind_of?(Array) && temp.length > 0 && min_id!=nil
-	puts " (#{counter})"
-
-
-    print "Sent Tweets:          "
-	parameters = {"count"=>"200", "since_id"=>start}
-	min_id = nil
-	counter = 0
-	begin
-		temp = client.user_timeline(parameters)
-		print '#'
-		if temp.kind_of?(Array)
-			temp.each do |t|
-				tweet = Tweet.add(t)
-				print "."
-				counter+=1
-				if min_id==nil || min_id>tweet.tweet_id
-					min_id = tweet.tweet_id
-				end
-				objects << tweet
-			end
-		else
-			raise "Got error. Aborting."
-		end
-		parameters["max_id"] = min_id-1 rescue nil
-	end while temp.kind_of?(Array) && temp.length > 0 && min_id!=nil
-	puts " (#{counter})"
-
-
+	all_objects += get_tweets(client, :start=>start, :description=>"Timeline", :twitter_method=>:home_timeline, :tweet_add_method=>:add)
+	all_objects += get_tweets(client, :start=>start, :description=>"Mentions", :twitter_method=>:mentions, :tweet_add_method=>:add)
+	all_objects += get_tweets(client, :start=>start, :description=>"Sent Tweets", :twitter_method=>:user_timeline, :tweet_add_method=>:add)
 
     start = Tweet.maximum('tweet_id', :conditions=>'dm = 1') || 1
 
-    print "Received DMs:         "
-	parameters = {"count"=>"200", "since_id"=>start}
-	min_id = nil
-	counter = 0
-    begin
-        temp = client.messages(parameters)
-		print '#'
-		if temp.kind_of?(Array)
-			temp.each do |t|
-				tweet = Tweet.add_dm(t)
-				print "."
-				counter+=1
-				if min_id==nil || min_id>tweet.tweet_id
-					min_id = tweet.tweet_id
-				end
-				objects << tweet
-			end
-		else
-			raise "Got error. Aborting."
-		end
-		parameters["max_id"] = min_id-1 rescue nil
-    end while temp.kind_of?(Array) && temp.length > 0 && min_id!=nil
-	puts " (#{counter})"
+	all_objects += get_tweets(client, :start=>start, :description=>"Received DMs", :twitter_method=>:messages, :tweet_add_method=>:add_dm)
+	all_objects += get_tweets(client, :start=>start, :description=>"Sent DMs", :twitter_method=>:sent_messages, :tweet_add_method=>:add_dm)
 
-    print "Sent DMs:             "
-	parameters = {"count"=>"200", "since_id"=>start}
-	min_id = nil
-	counter = 0
-    begin
-        temp = client.sent_messages(parameters)
-		print '#'
-		if temp.kind_of?(Array)
-			temp.each do |t|
-				tweet = Tweet.add_dm(t)
-				print "."
-				counter+=1
-				if min_id==nil || min_id>tweet.tweet_id
-					min_id = tweet.tweet_id
-				end
-				objects << tweet
-			end
-		else
-			raise "Got error. Aborting."
-		end
-		parameters["max_id"] = min_id-1 rescue nil
-	end while temp.kind_of?(Array) && temp.length > 0 && min_id!=nil
-	puts " (#{counter})"
 
 	counter = 0
-	print "Saving data to DB:    "
-	objects.sort_by{|obj| obj.date}.each do |obj|
+	print "Saving data to DB:".ljust(25)
+	all_objects.sort_by{|obj| obj.date}.each do |obj|
 		if obj.save
 			counter += 1
 			print "."
@@ -154,5 +67,5 @@ def do_update(config)
 			print "#"
 		end
 	end
-	puts "  (#{counter}/#{objects.count})"
+	puts "  (#{counter}/#{all_objects.count})"
 end
